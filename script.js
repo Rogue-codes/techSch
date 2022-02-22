c00ba0c971d62ddb59fa73b84af3c4dbe2327905
Nnamdi Osuji <nosuji@saconsulting.ai>
2022-02-22 19:05:12 +0100

    techsc

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const toggle = document.querySelector('.toggle')
 
 const closebtn = document.querySelector('.closebtn')
 
+const fixedBtn = document.querySelector('.btn')
+
+const fixedBtn2 = document.querySelector('.btn2')
+
 
 
 toggle.addEventListener('click', ()=>{
@@ -14,4 +18,14 @@ toggle.addEventListener('click', ()=>{
 closebtn.addEventListener('click', ()=>{
     menu.style.left = '-100%'
     toggle.style.display = 'block'
+})
+
+window.addEventListener('load', ()=>{
+    fixedBtn.classList.add('fixed')
+    console.log('reload')
+})
+
+window.addEventListener('load', ()=>{
+    fixedBtn2.classList.add('fixed2')
+    console.log('reload')
 })
